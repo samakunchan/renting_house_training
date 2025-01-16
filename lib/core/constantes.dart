754d4bc14d3d1c64e0ec31faa4ddbb6c69dacac9
samakunchan <cedric.badjah@gmail.com>
2025-01-16 10:19:37 +0100

    feat: constantes
    - Updates font size for header and body.
    - Exported style for button.
    - Added theme for dropdown menu.

--- lib/core/constantes.dart
+++ lib/core/constantes.dart
@@ -13,19 +13,21 @@ const Color kSecondaryColor = Color(0xFF5BB2E4);
 const Color kTertiaryColor = Color(0xFF2F9FE1);
 final Color kShadowColor = Colors.grey.shade500;
 const Color kChipColor = Color(0xFF666666);
-const Color kTextShadowColor = Color(0xFF616161);
+const Color kTextShadowColor = Color(0xFF525252);
 const Color kLinearTopColor = Color(0xFF80c8f5);
 const Color kLinearBottomColor = Color(0xFF30a0e1);
 
 TextStyle kheadlineMedium = const TextStyle(
   fontFamily: raleway,
   fontWeight: FontWeight.w500,
+  fontSize: 20,
 );
 
 TextStyle kBodyMedium = const TextStyle(
   fontFamily: raleway,
   fontWeight: FontWeight.w300,
   color: kTextShadowColor,
+  fontSize: 16,
 );
 
 TextStyle kTextButtonMedium = const TextStyle(
@@ -44,13 +46,40 @@ TextStyle kTextChip = const TextStyle(
 
 final ThemeData kThemeData = ThemeData(
   appBarTheme: kAppBarTheme,
-  scaffoldBackgroundColor: kPrimaryColor,
+  scaffoldBackgroundColor: kLightColor,
   colorScheme: kColorScheme,
   textTheme: kTextTheme,
   textButtonTheme: kTextButtonThemeData,
   elevatedButtonTheme: kElevatedButtonThemeData,
   cardTheme: kCardThemeData,
-  iconTheme: const IconThemeData(color: kLightColor),
+
+  /// For sub menu of dropdown.
+  menuButtonTheme: MenuButtonThemeData(
+    style: ButtonStyle(
+      textStyle: WidgetStatePropertyAll(kheadlineMedium),
+      backgroundBuilder: (BuildContext context, Set<WidgetState> widgetState, Widget? child) {
+        if (widgetState.contains(WidgetState.disabled)) {
+          return Container(
+            decoration: const BoxDecoration(color: kLinearTopColor),
+            child: child,
+          );
+        }
+        return child!;
+      },
+      elevation: const WidgetStatePropertyAll<double>(0),
+    ),
+  ),
+  dropdownMenuTheme: DropdownMenuThemeData(
+    textStyle: kheadlineMedium,
+    inputDecorationTheme: const InputDecorationTheme(
+      border: InputBorder.none,
+    ),
+    menuStyle: MenuStyle(
+      backgroundColor: const WidgetStatePropertyAll<Color>(kLightColor),
+      shadowColor: WidgetStatePropertyAll<Color>(kShadowColor),
+      elevation: const WidgetStatePropertyAll<double>(1),
+    ),
+  ),
   useMaterial3: true,
 );
 
@@ -61,3 +90,38 @@ WidgetStateProperty<RoundedRectangleBorder> shape() {
     ),
   );
 }
+
+final ButtonStyle kButtonStyle = ButtonStyle(
+  shape: shape(),
+  textStyle: WidgetStateProperty.all<TextStyle>(kTextButtonMedium),
+  foregroundColor: WidgetStateProperty.resolveWith<Color?>(
+    (Set<WidgetState> states) {
+      if (states.contains(WidgetState.disabled)) {
+        return kShadowColor;
+      }
+      return kLightColor;
+    },
+  ),
+  backgroundBuilder: (BuildContext context, Set<WidgetState> widgetState, Widget? child) {
+    if (widgetState.contains(WidgetState.disabled)) {
+      return Container(
+        decoration: BoxDecoration(color: Colors.white.withValues(alpha: .5)),
+        child: child,
+      );
+    }
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: const LinearGradient(
+          colors: [
+            kLinearTopColor,
+            kLinearBottomColor,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: child,
+    );
+  },
+);
